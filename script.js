859f3fb6e7783ef9095b38135a25a0968e5cbabd
vippi-smart <mypcfor84@gmail.com>
2024-08-19 19:45:07 +0530

    dd

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// let canges= document.getElementById('canges');
+// const timings = () =>{
+//   setTimeout(()=>{
+//     canges.textContent="helper    ";
+//   },0); 
+//   setTimeout(()=>{
+//     canges.textContent="calculator";
+//   },4000);
+//   setTimeout(()=>{
+//     canges.textContent="mentor    ";
+//   },13000);
+
+  
+// } 
+//   timings();
+//    setInterval(timings,13000);
